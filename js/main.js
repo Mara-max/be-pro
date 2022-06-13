82058f8a1e6ccf1e8dccdfffae0a1199de9800b1
Mara-max <106429043+Mara-max@users.noreply.github.com>
2022-06-13 17:14:12 +0200

    Add files via upload
    additional files

---
+++ js/main.js
@@ -0,0 +1,68 @@
+//Toggler
+$(".nav-toggler").click(function () {
+    $(".nav-items").toggleClass("show");
+});
+
+//Hero Slider
+$('.hero-slider').owlCarousel({
+    loop: true,
+    nav: true,
+    dots: true,
+    items: 1,
+    smartSpeed: 1000,
+    navText: ["<i class='ri-arrow-left-line'></i>", "<i class='ri-arrow-right-line'></i>"]
+
+    /* responsive: {
+         0: {
+              items: 1
+          },
+          600: {
+              items: 3
+          },
+          1000: {
+              items: 5
+          }
+      }*/
+})
+
+//Portfolio Slider
+$('.portfolio-slider').owlCarousel({
+    loop: true,
+    nav: true,
+    dots: false,
+    smartSpeed: 1000,
+    margin: 24,
+    navText: ["<i class='ri-arrow-left-line'></i>", "<i class='ri-arrow-right-line'></i>"],
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 2
+        },
+        992: {
+            items: 3
+        }
+    }
+})
+
+//Review Slider
+$('.reviews-slider').owlCarousel({
+    loop: true,
+    nav: true,
+    dots: false,
+    smartSpeed: 1000,
+    margin: 24,
+    navText: ["<i class='ri-arrow-left-line'></i>", "<i class='ri-arrow-right-line'></i>"],
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 2
+        },
+        992: {
+            items: 3
+        }
+    }
+})
